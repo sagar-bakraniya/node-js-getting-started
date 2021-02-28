5ed3328041a78e4e946a9d62b606add15b0e6fe0
sagar bakraniya <sagarbakraniya5@gmail.com>
2021-02-28 13:35:09 +0530

    add socket

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const server = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const io = require("socket.io")(server); 
+  
+io.on('connection', (socket) => { 
+     console.log("New user connected"); 
+});
